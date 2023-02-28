02946f4dceca4d5d350832cac49ce63503be0e4b
Sergey <runix1981@gmail.com>
2023-02-28 10:07:14 +0300

    add task find result of max sub min elements of massive

---
+++ Task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+double[] Array(string msg)
+{
+    Console.Write($"{msg} -> ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void SubOfMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    System.Console.Write($"{array[0]} ");
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+        if (array[i] >= max) max = array[i];
+        if (array[i] <= min) min = array[i];
+    }
+    System.Console.WriteLine("");
+    double result = max - min;
+    System.Console.WriteLine($"Разница между максимальным значением массива {max} и минимальным значением {min} равна {result}.");
+}
+
+SubOfMaxMin(Array("Введите размер массива"));
